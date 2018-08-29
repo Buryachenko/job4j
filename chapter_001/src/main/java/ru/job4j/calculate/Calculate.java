7df20ee0089df1ffc6b4144586a9286bc85ad0ee
Buryachenko <mailto:ovburyachenko@yandex.ru>
2018-08-29 22:55:52 +0500

    1.3. JavaDoc. Code convention[#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,17 @@
 package ru.job4j.calculate;
+
+/**
+ *	Class Класс для вычисления арифметических операций + - * / ^.
+ *	@author Buryachenko
+ *	@since 29.08.2018
+ *	@version 1 
+ */
+
 public class Calculate {
+	/**
+	 *	Main.
+	 *	@param args - args.
+	 */
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
