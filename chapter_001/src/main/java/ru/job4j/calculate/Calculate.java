e2996f67a2a0e0311c44efe9dd0efe6feb7be329
Buryachenko <mailto:ovburyachenko@yandex.ru>
2018-08-30 22:17:40 +0500

    1.4. JUnit[#4122]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,18 +1,27 @@
 package ru.job4j.calculate;
 
 /**
- *	Class Класс для вычисления арифметических операций + - * / ^.
- *	@author Buryachenko
- *	@since 29.08.2018
- *	@version 1 
+ * Class Класс для вычисления арифметических операций + - * / ^.
+ * @author Buryachenko
+ * @since 29.08.2018
+ * @version 1 
  */
 
 public class Calculate {
 	/**
-	 *	Main.
-	 *	@param args - args.
+	 * Main.
+	 * @param args - args.
 	 */
 	public static void main(String[] args){
 		System.out.println("Hello World");
+	}
+
+	/**
+	 * Method echo.
+	 * @param name Your name.
+	 * @return Echo plus your name.
+	 */
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
 	}
 }
