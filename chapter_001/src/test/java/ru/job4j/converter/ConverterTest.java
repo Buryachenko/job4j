cf45c7478ab6ce957403100c1b15faf6f2bf2e70
Buryachenko <mailto:ovburyachenko@yandex.ru>
2018-09-08 21:54:55 +0500

    3.4. Расстояние между точками в системе координат[#188]

--- chapter_001/src/test/java/ru/job4j/converter/ConverterTest.java
+++ chapter_001/src/test/java/ru/job4j/converter/ConverterTest.java
@@ -22,14 +22,14 @@ public class ConverterTest {
 	@Test
     public void when1DollarToRubleThen60() {
         Converter converter = new Converter();
-        int result = converter.rubleToDollar(1);
+        int result = converter.dollarToRuble(1);
         assertThat(result, is(60));
     }
 
     @Test
     public void when1EuroToRubleThen70() {
         Converter converter = new Converter();
-        int result = converter.rubleToEuro(1);
+        int result = converter.euroToRuble(1);
         assertThat(result, is(70));
     }
 }
