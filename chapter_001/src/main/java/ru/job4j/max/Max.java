782197c9924fc51794ae5f89034e207a52b27ae6
Buryachenko <mailto:ovburyachenko@yandex.ru>
2018-09-16 00:59:03 +0500

    4.2. Максимум из двух чисел.[#189]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,19 @@
+package ru.job4j.max;
+
+/**
+ * Class Класс для вычисления максимума из двух чисел.
+ * @author Buryachenko
+ * @since 29.08.2018
+ * @version 1
+ */
+
+public class Max{
+	/**
+	 * Method max.
+	 * @param first, second.
+	 * @return max value.
+	 */
+	public int max(int first, int second){	
+		return first > second ? first : second;
+	}
+}	
