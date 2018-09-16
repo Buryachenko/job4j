4633d203c18d0564346404c768ca04e97b805398
Buryachenko <mailto:ovburyachenko@yandex.ru>
2018-09-16 21:15:48 +0500

    6.0. Заполнить массив степенями чисел. [#33488]

---
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+/**
+ * Class Класс заполняет массив степенями чисел.
+ * @author Buryachenko
+ * @since 16.09.2018
+ * @version 1
+ */
+ 
+public class Square {
+    public int[] calculate(int bound) {
+        int[] rst = new int[bound];
+		for(int i = 0; i < bound; i++){
+			rst[i] = (int)Math.pow((i + 1), 2);
+		}
+        return rst;
+    }
+}
