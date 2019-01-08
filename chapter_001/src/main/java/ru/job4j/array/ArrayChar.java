32516eda7fca41a2183d454c190aa3594ee42b90
Buryachenko <mailto:ovburyachenko@yandex.ru>
2019-01-08 22:07:22 +0500

    6.4. Слово начинается с ...[#71870]

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -1,15 +1,15 @@
 package ru.job4j.array;
-
 /**
  * Обертка над строкой.
+ *  @author Buryachenko
+ *  @since 08.01.2019
+ *  @version 1
  */
 public class ArrayChar {
     private char[] data;
-    
     public ArrayChar(String line) {
         this.data = line.toCharArray();
     }
-
     /**
      * Проверяет. что слово начинается с префикса.
      * @param prefix префикс.
@@ -19,10 +19,7 @@ public class ArrayChar {
         boolean result = true;
         char[] value = prefix.toCharArray();
         for(int i = 0; i < value.length; i++){
-			if(value[i] == this.data[i]){
-				result = true;
-			}
-			else {
+			if(value[i] != this.data[i]){
 				result = false;
 				break;
 			}
--- chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -3,7 +3,12 @@ package ru.job4j.array;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-
+/**
+ * Test.
+ * @author Oleg Buryachenko (mailto:ovburyachenko@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 public class ArrayCharTest {
     @Test
     public void whenStartWithPrefixThenTrue() {
@@ -11,7 +16,6 @@ public class ArrayCharTest {
         boolean result = word.startWith("He");
         assertThat(result, is(true));
     }
-
     @Test
     public void whenNotStartWithPrefixThenFalse() {
         ArrayChar word = new ArrayChar("Hello");
