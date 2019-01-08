1ea1d8fc5eb49e90d454d491533a6c205e2ea05f
Buryachenko <mailto:ovburyachenko@yandex.ru>
2019-01-08 21:11:02 +0500

    3.4. Расстояние между точками в системе координат.Добавил класс PointTest[#188]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,15 +1,12 @@
 package ru.job4j.condition;
- 
  /**
   * @author Oleg Buryachenko (mailto:ovburyachenko@yandex.ru)
   * @version $Id$
   * @since 0.1
   */
-  
 public class Point {
-   private int x;
-   private int y;
- 
+   public int x;
+   public int y;
    public  Point(int x, int y) {
     this.x = x;
     this.y = y;
@@ -21,26 +18,10 @@ public class Point {
     // Точка В - это входящая точка. К ней мы можем обратиться напрямую через имя переменной that.
     // или для удобства мы создали новую переменную b и к ней присвоили переменную that.
     Point b = that;
-    
 	int x1 = a.x;
     int y1 = a.y;
     int x2 = b.x;
-    int y2 = b.y; 
-	
+    int y2 = b.y;
     return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-  }
-  
-  public static void main(String[] args) {
-    Point a = new Point(0, 1);
-    Point b = new Point(2, 5);
-	
-	System.out.println("x1 = " + a.x);
-	System.out.println("y1 = " + a.y);
-	System.out.println("x2 = " + b.x);
-	System.out.println("y2 = " + b.y);
-	
-	double result = a.distanceTo(b);
-	
-	System.out.println("Расстояние между точками А и В : " + result);
   }
 }
---
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+/**
+ * @author Oleg Buryachenko (mailto:ovburyachenko@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class PointTest {
+    @Test
+    public void calculateDistanceAtoB() {
+        Point a = new Point(0, 1);
+        Point b = new Point(2, 5);
+
+        System.out.println("x1 = " + a.x);
+        System.out.println("y1 = " + a.y);
+        System.out.println("x2 = " + b.x);
+        System.out.println("y2 = " + b.y);
+
+        double result = a.distanceTo(b);
+
+        System.out.println("Расстояние между точками А и В : " + result);
+    }
+}
