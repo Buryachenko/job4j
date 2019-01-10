eb859cc85e66ae705c9e27219ab8cadb982945c1
Buryachenko <mailto:ovburyachenko@yandex.ru>
2019-01-11 01:17:49 +0500

    3.4. Расстояние между точками в системе координат[#70369] Поправил тест.

--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.condition;
 
 import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * @author Oleg Buryachenko (mailto:ovburyachenko@yandex.ru)
@@ -12,14 +14,8 @@ public class PointTest {
     public void calculateDistanceAtoB() {
         Point a = new Point(0, 1);
         Point b = new Point(2, 5);
-
-        System.out.println("x1 = " + a.x);
-        System.out.println("y1 = " + a.y);
-        System.out.println("x2 = " + b.x);
-        System.out.println("y2 = " + b.y);
-
+        double formula = Math.sqrt(Math.pow(2 - 0, 2) + Math.pow(5 - 1, 2));
         double result = a.distanceTo(b);
-
-        System.out.println("Расстояние между точками А и В : " + result);
+        assertThat(result, is(formula));
     }
 }
