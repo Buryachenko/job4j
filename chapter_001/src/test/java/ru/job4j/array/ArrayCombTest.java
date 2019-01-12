e61e3a660b3ab927ac16bb05c9c9d55e5ea13fa3
Buryachenko <mailto:ovburyachenko@yandex.ru>
2019-01-13 00:35:37 +0500

    Дополнительное задание. Базовый синтаксис.[#104532]

--- chapter_001/src/test/java/ru/job4j/array/ArrayCombTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCombTest.java
@@ -22,4 +22,28 @@ public class ArrayCombTest {
         int[] expect = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         assertThat(result, is(expect));
     }
+    @Test
+    public void whenArrayFirstLarge() {
+        int[] first = new int[] {1, 5, 4, 2, 3, 6, 9, 8, 10, 7};
+        int[] second = new int[] {11, 13, 14, 15, 12};
+        BubbleSort bubble = new BubbleSort();
+        int[] arrayFirst = bubble.sort(first);
+        int[] arraySecond = bubble.sort(second);
+        ArrayComb comb = new ArrayComb();
+        int[] result = comb.combiningTwoArrayToOne(arraySecond, arrayFirst);
+        int[] expect = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenArraySecondLarge() {
+        int[] first = new int[] {15, 13, 14};
+        int[] second = new int[] {1, 2, 3, 9, 4, 6, 7, 10, 5, 11, 8, 12};
+        BubbleSort bubble = new BubbleSort();
+        int[] arrayFirst = bubble.sort(first);
+        int[] arraySecond = bubble.sort(second);
+        ArrayComb comb = new ArrayComb();
+        int[] result = comb.combiningTwoArrayToOne(arraySecond, arrayFirst);
+        int[] expect = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        assertThat(result, is(expect));
+    }
 }
