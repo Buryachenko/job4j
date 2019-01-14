f91b3f8ffccd5407d7b75de523673e2f2e9b0f77
Buryachenko <mailto:ovburyachenko@yandex.ru>
2019-01-14 21:27:03 +0500

    5. Рефакторинг теста @Before @After [#33568] [#106347]

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -1,4 +1,6 @@
 package ru.job4j.pseudo;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -12,44 +14,51 @@ import static org.junit.Assert.assertThat;
  * @since 0.1
  */
 public class PaintTest {
+
+    private final PrintStream stdout = System.out;
+
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         assertThat(
-                    new String(out.toByteArray()),
-                        is(
-                            new StringBuilder()
-                            .append("+++++++")
-                            .append("+++++++")
-                            .append("+++++++")
-                            .append("+++++++")
-                            .append(System.lineSeparator())
-                            .toString()
-                        )
-                  );
-        System.setOut(stdout);
+                new String(out.toByteArray()),
+                is(
+                        new StringBuilder()
+                                .append("+++++++")
+                                .append("+++++++")
+                                .append("+++++++")
+                                .append("+++++++")
+                                .append(System.lineSeparator())
+                                .toString()
+                )
+        );
     }
     @Test
     public void whenDrawTriangle() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Triangle());
         assertThat(
-                    new String(out.toByteArray()),
-                        is(
-                            new StringBuilder()
-                            .append("+")
-                            .append("+++")
-                            .append("+++++")
-                            .append("+++++++")
-                            .append(System.lineSeparator())
-                            .toString()
-                        )
-                  );
-        System.setOut(stdout);
+                new String(out.toByteArray()),
+                is(
+                        new StringBuilder()
+                                .append("+")
+                                .append("+++")
+                                .append("+++++")
+                                .append("+++++++")
+                                .append(System.lineSeparator())
+                                .toString()
+                )
+        );
     }
 }
