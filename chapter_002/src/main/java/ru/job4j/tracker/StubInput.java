7f8ae6b16d592b5278ab0454047b2b62097b2abf
Buryachenko <mailto:ovburyachenko@yandex.ru>
2019-01-21 22:29:02 +0500

    1. Обеспечить бесперебойную работу приложения Tracker. [#789] [#107650]

--- chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
@@ -18,6 +18,18 @@ public class StubInput implements Input {
 
     @Override
     public int ask(String question, int[] range) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        int key = Integer.valueOf(this.ask(question));
+        boolean exist = false;
+        for (int value : range) {
+            if (value == key) {
+                exist = true;
+                break;
+            }
+        }
+        if (exist) {
+            return key;
+        } else {
+            throw new MenuOutException("Out of menu range.");
+        }
     }
 }
