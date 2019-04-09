793b79eba933afcdd336be9114a8fc090c0278e5
Buryachenko <mailto:ovburyachenko@yandex.ru>
2019-04-09 22:45:17 +0500

    5.3.3. Используя контейнер на базе связанного списка создать контейнер Stack[#129151]

---
+++ junior_chapter_001/src/main/java/ru/job4j/list/SimpleQueue.java
@@ -0,0 +1,28 @@
+package ru.job4j.list;
+/**
+ *
+ * Class Класс контейнера типа Queue, реализованный на двух объектах типа SimpleStack
+ * @athor Oleg Buryachenko
+ * @since 09.04.19
+ * @version 1
+ */
+public class SimpleQueue<T> {
+    private SimpleStack<T> stackFirst;
+    private SimpleStack<T> stackSecond;
+    public SimpleQueue() {
+        stackFirst = new SimpleStack<>();
+        stackSecond = new SimpleStack<>();
+    }
+    public T poll() {
+        if(stackSecond.getSize() == 0) {
+            while (stackFirst.getSize() != 0) {
+                stackSecond.push(stackFirst.poll());
+            }
+        }
+        return stackSecond.poll();
+    }
+
+    public void push(T value) {
+        stackFirst.push(value);
+    }
+}
---
+++ junior_chapter_001/src/test/java/ru/job4j/list/SimpleQueueTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.list;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+/**
+ *
+ * @Test
+ * @athor Oleg Buryachenko (mailto: ovburyachenko@yandex.ru)
+ * @since 0.1
+ * @version $Id$
+ */
+public class SimpleQueueTest {
+
+    @Test(expected = NullPointerException.class)
+    public void whenPushThreeElementsThenResultStackData() {
+        SimpleQueue<Integer> simpleQueue = new SimpleQueue<>();
+        simpleQueue.push(1);
+        simpleQueue.push(2);
+        simpleQueue.push(3);
+        assertThat(simpleQueue.poll(), is(1));
+        simpleQueue.push(4);
+        assertThat(simpleQueue.poll(), is(2));
+        assertThat(simpleQueue.poll(), is(3));
+        assertThat(simpleQueue.poll(), is(4));
+        simpleQueue.poll();
+    }
+}
