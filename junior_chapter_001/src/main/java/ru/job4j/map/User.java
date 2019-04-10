ef0a836d0863385d0418dc0458cb8836a28afde7
Buryachenko <mailto:ovburyachenko@yandex.ru>
2019-04-10 14:49:20 +0500

    1. Создать модель User[#129170]

---
+++ junior_chapter_001/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,15 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+/**
+ *
+ * Class Класс User
+ * @athor Oleg Buryachenko
+ * @since 10.04.19
+ * @version 1
+ */
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+}
