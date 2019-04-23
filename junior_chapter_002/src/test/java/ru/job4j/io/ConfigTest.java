158d873b8ccd3c45f31275d31937efc385d92c28
Buryachenko <mailto:ovburyachenko@yandex.ru>
2019-04-23 22:18:21 +0500

    1. Читаем файл конфигурации[#129189]

--- junior_chapter_002/src/test/java/ru/job4j/io/ConfigTest.java
+++ junior_chapter_002/src/test/java/ru/job4j/io/ConfigTest.java
@@ -3,16 +3,22 @@ package ru.job4j.io;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
-
+/**
+ *
+ * @Test
+ * @athor Oleg Buryachenko (mailto: ovburyachenko@yandex.ru)
+ * @since 0.1
+ * @version $Id$
+ */
 public class ConfigTest {
     @Test
     public void whenLoadProperties() {
-        Config config = new Config("C:\\projects\\job4j\\app.properties");
+        Config config = new Config("app.properties");
         config.load();
-        assertThat(config.value("hibernate.dialect"), is("org.hibernate.dialect.PostgreSQLDialect"));
-        assertThat(config.value("hibernate.connection.url"), is("jdbcpostgresql127.0.0.15432trackstudio"));
-        assertThat(config.value("hibernate.connection.driver_class"), is("org.postgresql.Driver"));
-        assertThat(config.value("hibernate.connection.username"), is("postgres"));
-        assertThat(config.value("hibernate.connection.password"), is("password"));
+        assertThat(config.value("hibernate.dialect"),                   is("org.hibernate.dialect.PostgreSQLDialect"));
+        assertThat(config.value("hibernate.connection.url"),            is("jdbcpostgresql127.0.0.15432trackstudio"));
+        assertThat(config.value("hibernate.connection.driver_class"),   is("org.postgresql.Driver"));
+        assertThat(config.value("hibernate.connection.username"),       is("postgres"));
+        assertThat(config.value("hibernate.connection.password"),       is("password"));
     }
 }
