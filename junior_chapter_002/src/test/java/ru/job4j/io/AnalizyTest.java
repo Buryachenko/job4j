c1a9ca5da03452aa9eff165c043ad1e4181f64d9
Buryachenko <mailto:ovburyachenko@yandex.ru>
2019-04-23 23:20:40 +0500

    2. Анализ доступности сервера.[#129190]

--- junior_chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
+++ junior_chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,7 +1,9 @@
 package ru.job4j.io;
 import org.junit.Test;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Iterator;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -13,19 +15,24 @@ import static org.junit.Assert.*;
  * @version $Id$
  */
 public class AnalizyTest {
-    String source = "C:\\projects\\job4j\\source.txt";
-    String target = "C:\\projects\\job4j\\target.txt";
-
+    String source = "source.txt";
+    String target = "target.txt";
+//
     @Test
-    public void whenWriteTwoStrOfTarget() {
+    public void whenWriteTwoStrOfTarget() throws IOException {
         Analizy analizy = new Analizy();
         analizy.unavailable(source, target);
         try (BufferedReader read = new BufferedReader(new FileReader(target))) {
             Iterator<String> reader = read.lines().iterator();
             assertThat(reader.next(), is("10:57:01;10:59:01"));
             assertThat(reader.next(), is("11:01:02;11:02:02"));
-        } catch (Exception e) {
-            e.printStackTrace();
+        }
+    }
+
+    @Test(expected = FileNotFoundException.class)
+    public void whenFileNotFoundException() throws IOException {
+        try (BufferedReader read = new BufferedReader(new FileReader("not file"))) {
+            read.lines();
         }
     }
 }
