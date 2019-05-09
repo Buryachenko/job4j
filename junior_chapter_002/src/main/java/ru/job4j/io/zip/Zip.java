78ac35ae4131a3a435dc7381a719d4bfbe2daded
Buryachenko <mailto:ovburyachenko@yandex.ru>
2019-05-10 00:41:18 +0500

    4. Архивировать проект. [#129191]

--- junior_chapter_002/src/main/java/ru/job4j/io/zip/Zip.java
+++ junior_chapter_002/src/main/java/ru/job4j/io/zip/Zip.java
@@ -2,6 +2,9 @@ package ru.job4j.io.zip;
 
 import ru.job4j.io.Search;
 import java.io.*;
+import java.net.URI;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -17,6 +20,7 @@ public class Zip {
     File target;
     List<String> include;
     Search search = new Search();
+    private Path base = null;
 
     public Zip() {
 
@@ -24,10 +28,12 @@ public class Zip {
 
     public Zip(String[] args) {
         this.arguments = new Args(args);
+        this.base = Paths.get(this.arguments.directory()).getRoot();
         this.source = new File(this.arguments.directory());
         this.target = new File(this.arguments.output());
         this.include = arguments.include();
         System.out.println(arguments);
+        System.out.println(base);
     }
 
     public void run() {
@@ -39,13 +45,14 @@ public class Zip {
     }
 
     public void pack(File source, File target) {
+        this.base = source.toPath().getRoot();
         pack(List.of(source), target);
     }
 
     private void pack(List<File> filesInclude, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
             for (File file : filesInclude) {
-                zip.putNextEntry(new ZipEntry(file.getPath()));
+                zip.putNextEntry(new ZipEntry(this.base.relativize(file.toPath()).toString()));
                 try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(file))) {
                     zip.write(out.readAllBytes());
                 }
